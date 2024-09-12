9c185bdbcc71c6174c7d915b6dbc998fc524ba9b
WashingtonJR <washington.junior.ibv@gmail.com>
2024-09-12 15:49:11 -0300

    ajuste em um if que estava dando incorreto

--- Avaliação/atualizarContato.js
+++ Avaliação/atualizarContato.js
@@ -5,7 +5,17 @@ function atualizarContato(id, novosDados) {
   const contatos = getContatos();
   const index = contatos.findIndex(contato => contato.id === id);
   if (index !== -1) {
-    contatos[index] = { ...contatos[index], ...novosDados };
+    
+    if (novosDados.nome) {
+      contatos[index].nome = novosDados.nome;
+    }
+    if (novosDados.telefone) {
+      contatos[index].telefone = novosDados.telefone;
+    }
+    if (novosDados.email) {
+      contatos[index].email = novosDados.email;
+    }
+    
     setContatos(contatos);
   }
 }
